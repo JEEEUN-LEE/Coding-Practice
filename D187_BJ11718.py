3709580ccebdf136fe96a071b0e6b8468b454f20
JEEEUN LEE <jee4390@gmail.com>
2025-05-13 23:25:49 +0900

    practice upload

---
+++ D187_BJ11718.py
@@ -0,0 +1,11 @@
+# 백준 11718번: 그대로 출력하기
+# 문제: https://www.acmicpc.net/problem/11718
+# 문제: 입력받은 문자열을 그대로 출력하는 문제
+
+while True:
+    try:
+        print(input()) 
+    except EOFError: # EOFError는 파일의 끝에 도달했을 때 발생하는 예외외
+        break
+# 입력을 계속 받아 출력하다가 EOFError가 발생하면 반복문을 종료
+
