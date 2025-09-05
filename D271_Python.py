11c5c83f53efeab48999b224890d306dd2632bd1
JEEEUN LEE <jee4390@gmail.com>
2025-09-05 23:07:43 +0900

    practice upload

---
+++ D271_Python.py
@@ -0,0 +1,13 @@
+# 프로그래머스: [PCCE 기출문제] 1번 / 출력
+# Spring is beginning
+# 13
+# 310 => 이렇게 출력되기 위한 코드 작성하기
+
+
+string_msg = "Spring is beginning"
+int_val = 3
+string_val = "3"
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
